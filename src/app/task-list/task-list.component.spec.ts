e267c0d783c2b8ec948b061137f238b61a1d395c
Eric Zimmer <eric.zimmer@mymail.champlain.edu>
2021-05-09 16:32:44 -0600

    Added test to TaskList component

--- src/app/task-list/task-list.component.spec.ts
+++ src/app/task-list/task-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TaskListComponent } from './task-list.component';
+import { Task } from '../task';
+import { By } from '@angular/platform-browser';
 
 describe('TaskListComponent', () => {
   let component: TaskListComponent;
@@ -21,5 +22,16 @@ describe('TaskListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display newly added tasks', () => {
+    let tasksCount = fixture.debugElement.queryAll(By.css('app-task')).length;
+    expect(tasksCount).toBe(2, 'incorrect number of default tasks rendered');
+
+    component.tasks.push(new Task(3, 'Sample Test 3', new Date('05-9-21'), 'High', 'I\'m the third task!'));
+    fixture.detectChanges();
+
+    tasksCount = fixture.debugElement.queryAll(By.css('app-task')).length;
+    expect(tasksCount).toBe(3, 'newly added task not rendered');
   });
 });
