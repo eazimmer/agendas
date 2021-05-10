451b49f74221246e1c27bef7b222e14fa76fa2c5
Eric Zimmer <eric.zimmer@mymail.champlain.edu>
2021-05-10 15:00:29 -0600

    Added test for TaskToday component

--- src/app/task-today/task-today.component.spec.ts
+++ src/app/task-today/task-today.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskTodayComponent } from './task-today.component';
+import {Task} from '../task';
 
 describe('TasksTodayComponent', () => {
   let component: TaskTodayComponent;
@@ -21,5 +22,12 @@ describe('TasksTodayComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display only tasks due with today\'s date', () => {
+    const todaysDate = new Date().toString();
+    expect(component.tasks.find(task => task.dueDate.toString() === todaysDate)).toBe(undefined, 'a task was incorrectly initialized with today\'s date');
+    component.tasks.push(new Task(3, 'Today\'s Task', new Date(), 'High', 'Description of today\'s task'));
+    expect(component.tasks.find(task => task.dueDate.toString() === todaysDate)).not.toBe(undefined, 'a task with today\'s due date wasn\'t found, but should have been');
   });
 });
